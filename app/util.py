08be62c9a34170430ec289e4f663161984607e01
Javier Dominguez <javierdominguez@iMac-de-Javier.local>
2021-03-17 10:27:54 +0100

    merge upstream

--- app/util.py
+++ app/util.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-# pylint: disable=no-else-return
-def convert_to_number(operand):
-    try:
-        if "." in operand:
-            return float(operand)
-        else:
-            return int(operand)
-
-    except ValueError:
-        raise TypeError("Operator cannot be converted to number")
-
-
-def InvalidConvertToNumber(operand):
-    try:
-        if "." in operand:
-            return (float(operand))
-
-        return int(operand)
-
-    except ValueError:
-        raise TypeError("Operator cannot be converted to number")
-=======
 # pylint: disable=no-else-return
 def convert_to_number(operand):
     try:
@@ -47,4 +24,3 @@ def InvalidConvertToNumber(operand):
 def validate_permissions(operation, user):
     print(f"checking permissions of {user} for operation {operation}")
     return user == "user1"
->>>>>>> upstream/master
--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-import unittest
-import pytest
-
-from app.calc import Calculator
-
-
-@pytest.mark.unit
-class TestCalculate(unittest.TestCase):
-    def setUp(self):
-        self.calc = Calculator()
-
-    def test_add_method_returns_correct_result(self):
-        self.assertEqual(4, self.calc.add(2, 2))
-        self.assertEqual(0, self.calc.add(2, -2))
-        self.assertEqual(0, self.calc.add(-2, 2))
-        self.assertEqual(1, self.calc.add(1, 0))
-
-    def test_divide_method_returns_correct_result(self):
-        self.assertEqual(1, self.calc.divide(2, 2))
-        self.assertEqual(1.5, self.calc.divide(3, 2))
-
-    def test_add_method_fails_with_nan_parameter(self):
-        self.assertRaises(TypeError, self.calc.add, "2", 2)
-        self.assertRaises(TypeError, self.calc.add, 2, "2")
-        self.assertRaises(TypeError, self.calc.add, "2", "2")
-        self.assertRaises(TypeError, self.calc.add, None, 2)
-        self.assertRaises(TypeError, self.calc.add, 2, None)
-        self.assertRaises(TypeError, self.calc.add, object(), 2)
-        self.assertRaises(TypeError, self.calc.add, 2, object())
-
-    def test_divide_method_fails_with_nan_parameter(self):
-        self.assertRaises(TypeError, self.calc.divide, "2", 2)
-        self.assertRaises(TypeError, self.calc.divide, 2, "2")
-        self.assertRaises(TypeError, self.calc.divide, "2", "2")
-
-    def test_divide_method_fails_with_division_by_zero(self):
-        self.assertRaises(TypeError, self.calc.divide, 2, 0)
-        self.assertRaises(TypeError, self.calc.divide, 2, -0)
-        self.assertRaises(TypeError, self.calc.divide, 0, 0)
-        self.assertRaises(TypeError, self.calc.divide, "0", 0)
-
-
-if __name__ == "__main__":  # pragma: no cover
-    unittest.main()
-=======
 import unittest
 from unittest.mock import patch
 import pytest
@@ -100,4 +54,3 @@ class TestCalculate(unittest.TestCase):
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
->>>>>>> upstream/master
